de954f2e2fd2c023e7038326a75f35e8baf683aa
Aashan M <a2meiyap@edu.uwaterloo.ca>
2020-03-09 15:17:00 -0400

    Updated inserting for 500 files

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-var faker = require('faker');
 var mysql = require('mysql');
-
+var faker = require('faker');
+ 
+ 
 var connection = mysql.createConnection({
-	host	: 'localhost',
-	user	: 'root',
-	password: 'password',
-	database: 'enter_ly'
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'enter_ly'
 });
-
-var q = 'SELECT COUNT(*) AS total FROM users';
-connection.query(q, function (error, results, fields) {
-  if (error) throw error;
-  console.log(results[0].total);
+ 
+ 
+var data = [];
+for(var i = 0; i < 500; i++){
+    data.push([
+        faker.internet.email(),
+        faker.date.past()
+    ]);
+}
+ 
+ 
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+ 
+connection.query(q, [data], function(err, result) {
+  console.log(err);
+  console.log(result);
 });
-
+ 
 connection.end();
