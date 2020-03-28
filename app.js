20486ea1ea36714af0f024295504f1ac3fcb752c
Aashan M <a2meiyap@edu.uwaterloo.ca>
2020-03-28 14:19:05 -0400

    Set up web application framework

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var mysql = require('mysql');
 var faker = require('faker');
- 
+var express = require('express');
+var app = express();
+
  
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -8,22 +10,13 @@ var connection = mysql.createConnection({
   password : 'password',
   database : 'enter_ly'
 });
+  
+app.get("/", function(req, res){
+ res.send("HELLO FROM OUR WEB APP!");
+});
  
- 
-var data = [];
-for(var i = 0; i < 500; i++){
-    data.push([
-        faker.internet.email(),
-        faker.date.past()
-    ]);
-}
- 
- 
-var q = 'INSERT INTO users (email, created_at) VALUES ?';
- 
-connection.query(q, [data], function(err, result) {
-  console.log(err);
-  console.log(result);
+app.listen(8080, function () {
+ console.log('App listening on port 8080!');
 });
  
 connection.end();
