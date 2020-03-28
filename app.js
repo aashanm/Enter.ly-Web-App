5dabdd5850c0dee4b505b1bf86168a62b610ea01
Aashan M <a2meiyap@edu.uwaterloo.ca>
2020-03-28 14:28:05 -0400

    Connected form using register post route

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ app.get("/", function(req, res){
  res.send(msg);
  });
 });
+
+app.post('/register', function(req,res){
+ var person = {email: req.body.email};
+ connection.query('INSERT INTO users SET ?', person, function(err, result) {
+ console.log(err);
+ console.log(result);
+ res.redirect("/");
+ });
+});
  
 app.listen(8080, function () {
  console.log('App listening on port 8080!');
